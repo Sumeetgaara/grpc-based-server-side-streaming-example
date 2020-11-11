d701657361e8ab3bc64034d78b90fb6ed4fd3c63
sumbagaara <sumbagaara@gmail.com>
2020-11-11 22:34:18 +0530

    initial commit

---
+++ realtimeapi/realtimeapi/Services/StreamingService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using realtimeapi.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace realtimeapi.Services
+{
+    public class StreamingService : streaming.streamingBase
+    {
+        public override async Task StreamingFromServer(streamrequest req, IServerStreamWriter<streamresponse> responseStream, ServerCallContext context)
+        {
+            streamresponse res = new streamresponse();
+            while(true)
+            {
+                res.Response = "generate random strings";
+                await responseStream.WriteAsync(res);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
